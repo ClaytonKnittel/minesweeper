58e47b3f46f85e3ad3bc389d25952359316bfbab
Clayton Knittel <cknit1999@gmail.com>
2025-05-24 21:33:38 -0700

    Window appears

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-use bevy::app::App;
+use bevy::{
+  DefaultPlugins,
+  app::App,
+  prelude::PluginGroup,
+  utils::default,
+  window::{PresentMode, Window, WindowPlugin},
+};
 
 fn main() {
-  App::new().run();
+  App::new()
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+      primary_window: Some(Window {
+        title: "Minesweeper".into(),
+        name: Some("minesweeper.app".into()),
+        present_mode: PresentMode::AutoVsync,
+        prevent_default_event_handling: false,
+        ..default()
+      }),
+      ..default()
+    }))
+    .run();
 }
